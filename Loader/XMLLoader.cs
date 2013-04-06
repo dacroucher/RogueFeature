a4e31c2ccd3cb9eb840ec149b7e2653c64ecc8ee
rgiesler <rgiesler@gmail.com>
2013-04-06 23:50:39 +0200

    Some last minute changes that merging broke

--- Loader/XMLLoader.cs
+++ Loader/XMLLoader.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
-using Backend;
+using RogueFeature.Backend;
 
 namespace Loader
 {
@@ -33,19 +33,19 @@ namespace Loader
             switch (dir)  //convert number in xml to direction enum
             {
                 case 0:
-                    return UP;
+                    return Direction.UP;
                     break;
                 case 1:
-                    return RIGHT;
+                    return Direction.RIGHT;
                     break;
                 case 2:
-                    return DOWN;
+                    return Direction.DOWN;
                     break;
                 case 3:
-                    return LEFT;
+                    return Direction.LEFT;
                     break;
                 default:
-                    return UP;
+                    return Direction.UP;
                     break;
             }
         }
@@ -54,13 +54,13 @@ namespace Loader
         {
             XDocument doc = new XDocument();
             doc = XDocument.Load(_XMLFileName);
-            int rows, columns;
+            uint rows, columns;
 
             //GET: Each level
             foreach (XElement map in doc.Descendants("map"))    
             {
-                rows = Convert.ToInt32(map.Descendants("rows").FirstOrDefault().Value);
-                columns = Convert.ToInt32(map.Descendants("columns").FirstOrDefault().Value);
+                rows = Convert.ToUInt32(map.Descendants("rows").FirstOrDefault().Value);
+                columns = Convert.ToUInt32(map.Descendants("columns").FirstOrDefault().Value);
                 _map.Add(new Map(rows, columns));       //Create a new map, size rows x columns
                 Map latestMap = _map[_map.Count - 1];   //last added map reference
                 //GET: Each point
@@ -71,18 +71,18 @@ namespace Loader
                     string imagePath = point.Descendants("imagePath").FirstOrDefault().Value;
                     int direction = Convert.ToInt32(point.Descendants("direction").FirstOrDefault().Value);
                     bool passable = Convert.ToBoolean(point.Descendants("passable").FirstOrDefault().Value);
-                    latestMap.InitPoint(xPosiiton, yPosition, imagePath, intToDirection(direction), passable);  //Initialize point
+                    latestMap.InitPoint(xPosition, yPosition, imagePath, intToDirection(direction), passable);  //Initialize point
                     //GET: All units at point
                     foreach (XElement unit in point.Descendants("object").ToList())   
                     {
-                        string unitid = unit.Attribute("id");
+                        string unitid = unit.Attribute("id").Value;
                         XElement theunit = (from el in doc.Elements("object")
                          where (string)el.Attribute("id") == unitid
                          select el).FirstOrDefault();
-                        type = theunit.Descendants("type").FirstOrDefault().Value;
+                        string type = theunit.Descendants("type").FirstOrDefault().Value;
                         switch (type)
                         {
-                            case Unit:
+                            case "Unit":
 
                                 break;
                         }
